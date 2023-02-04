7e2f03619572bdd8eb7a2ed799582efd865268ea
chananababa <ephong93@gmail.com>
2023-02-05 01:36:23 +0900

    fix: move utils to root

--- src/utils.tsx
+++ src/utils.tsx
@@ -48,3 +48,11 @@ const processor = unified().use(rehypeReact, {
 export const renderAst = (ast: Node): JSX.Element => {
   return processor.stringify(ast as Root);
 };
+
+export const removeDuplicatesInArray = <T,>(arr: T[]): T[] => {
+  return Array.from(new Set(arr));
+};
+
+export const includesArray = <T,>(arr1: T[], arr2: T[]): boolean => {
+  return arr2.every((v) => arr1.includes(v));
+};
