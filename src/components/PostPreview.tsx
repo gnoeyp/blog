250505e35deaa1acee6e0e0a64ba7c17a414dcb5
chananababa <ephong93@gmail.com>
2023-02-05 00:57:04 +0900

    feat: text style of PostPreview

--- src/components/PostPreview.tsx
+++ src/components/PostPreview.tsx
@@ -8,8 +8,8 @@ type PostPreviewProps = {
 const PostPreview = ({ title, date }: PostPreviewProps) => {
   return (
     <div className="border rounded-lg p-3 hover:bg-gray-50">
-      <div>{title}</div>
-      <div>{date}</div>
+      <div className="font-medium">{title}</div>
+      <div className="text-sm text-gray-500">{date}</div>
     </div>
   );
 };
