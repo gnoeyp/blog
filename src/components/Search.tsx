250da7c255cd083fda3ec165f34681253e1c8628
chananababa <ephong93@gmail.com>
2023-02-10 23:43:46 +0900

    feat: improve search bar ui

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 
 type SearchProps = {
@@ -7,13 +9,20 @@ type SearchProps = {
 
 const Search = ({ onChange, value }: SearchProps) => {
   return (
-    <input
-      className="w-96 h-10 px-3 border rounded-lg border-gray-300 outline-none focus:border-gray-500"
-      value={value}
-      onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-        onChange?.(e.target.value)
-      }
-    />
+    <div className="relative">
+      <input
+        className="w-96 h-10 px-10 border rounded-full border-gray-300 outline-none outline-offset-0 focus:outline-lime-100 focus:outline-2 text-gray-900"
+        value={value}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          onChange?.(e.target.value)
+        }
+        placeholder="Search posts"
+      />
+      <FontAwesomeIcon
+        className="absolute left-4 top-3 text-gray-500"
+        icon={faMagnifyingGlass}
+      />
+    </div>
   );
 };
 
