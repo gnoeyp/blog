df11a89089038bc87be2639484b5d7e41a601ce1
chananababa <ephong93@gmail.com>
2023-02-19 00:02:46 +0900

    fix: manually load fontawesome css to remove initial flash

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -4,6 +4,10 @@ import "./src/styles/prism-solarizedlight.css";
 import React from "react";
 import Layout from "./src/components/Layout";
 import type { GatsbyBrowser } from "gatsby";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+
+config.autoAddCss = false;
 
 export const wrapPageElement: GatsbyBrowser["wrapPageElement"] = ({
   element,
